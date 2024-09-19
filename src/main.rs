beb3ecbf7589f5a1d1312bc58fa67fc9f8433771
Laura <0xaa55@8086.re>
2024-09-19 17:32:33 +0200

    UPDATE : Main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+extern crate clap;
+
+use clap::{Arg, App};
 fn main() {
-    println!("Hello, world!");
+    let matches = App::new("mewGet")
+        .version("1.0")
+        .author("Kitty")
+        .about("wget for cats")
+        .arg(Arg::with_name("url")
+            .required(true)
+            .takes_value(true)
+            .index(1)
+            .help("url to dl"))
+        .get_matches();
+    let mew = matches.value_of("url").unwrap();
+    println!("{}", mew);
 }
